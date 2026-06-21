7234affded23c2e6425be89ddc081541a3cc56fe
hadeedhussainmemon <programmerhadeed@gmail.com>
2026-06-21 13:29:29 +0500

    generate and configure premium Open Graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,28 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "Nur E Qalbb",
   },
+  openGraph: {
+    title: "Nur E Qalbb",
+    description: "An elegant, premium, distraction-free Islamic productivity platform.",
+    url: "https://nur-e-qulb.vercel.app",
+    siteName: "Nur E Qalbb",
+    images: [
+      {
+        url: "/og.png",
+        width: 1200,
+        height: 630,
+        alt: "Nur E Qalbb Social Sharing Banner",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Nur E Qalbb",
+    description: "An elegant, premium, distraction-free Islamic productivity platform.",
+    images: ["/og.png"],
+  },
 };
 
 export const viewport = {
